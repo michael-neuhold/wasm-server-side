8fbfd171f38ed7a14e3447011b09cf99bf713a68
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-09-10 17:21:36 +0200

    added example with static files

---
+++ example4/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Content-Type: text/html");
+    println!();
+    println!("<html>
+        <head>
+            <link rel=\"stylesheet\" href=\"static/styles.css\">
+        </head> 
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    </html>");
+}
